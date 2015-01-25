8d5e9cc0b00e06e51b51b87884d99a2a49e94d9d
nkosmicki <nicholaskosmicki@gmail.com>
2015-01-25 11:41:22 -0800

    Updates to matrix function
    Updated matrix function to perform matrix inverse and store result for
    future calls to function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# This function creates a list that contains 4 member functions: set, get, setInv
+# and getInv. It uses the <<- assignment operator so that these internal variables 
+# are not exposed to the outside environment.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  xInv <- NULL
+  set <- function(y) {
+    x <<- y
+    xInv <<- NULL
+    
+  }
+  get <- function() x
+  setInv <- function(Inv) xInv <<- Inv
+  getInv <- function() xInv
+  list(set = set, get = get, 
+       setInv = setInv,
+       getInv = getInv)
+  }
 
-}
 
-
-## Write a short comment describing this function
+# This function computes the inverse of the matrix returned by the
+# makeCacheMatrix function above. If the inverse has already been calculated 
+# and has not changed, then cacheSolve should retrieve the existing inverted 
+# matrix from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
